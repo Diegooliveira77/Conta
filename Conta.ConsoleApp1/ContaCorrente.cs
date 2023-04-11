ea837b4755e7570ff5d94ed44fcfd70a960a85e2
Diego <Diego@DESKTOP-VKT3JJP>
2023-04-11 17:04:01 -0300

    Add project files.

---
+++ Conta.ConsoleApp1/ContaCorrente.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Conta.ConsoleApp1
+{
+    public class ContaCorrente
+    {
+        public double Saldo { get; set; }
+        public int Numero { get; set; }
+        public double Limite { get; set; }
+        public bool Especial { get; set; }
+
+        public ArrayList Movimentacoes = new ArrayList();
+
+        public ContaCorrente(double saldo, int numero, double limite, bool especial)
+        {
+            Saldo = saldo;
+            Numero = numero;
+            Limite = limite;
+            Especial = especial;
+            Movimentacoes = new ArrayList();
+        }
+
+        public void Sacar(double valor, string tipo)
+        {
+            double limiteSaque = Limite + Saldo;
+
+            if (valor > limiteSaque)
+            {
+                Console.WriteLine("Saldo insuficiente para realizar o saque.");
+            }
+            else
+            {
+                Movimentacao movimentacao = new Movimentacao(-valor, tipo);
+                Movimentacoes.Add(movimentacao);
+                Saldo = Saldo - valor;
+            }
+        }
+
+        public void Depositar(double valor, string tipo)
+        {
+            Saldo += valor;
+            Movimentacao movimentacao = new Movimentacao(valor, tipo);
+            Movimentacoes.Add(movimentacao);
+        }
+        public void VerSaldo()
+        {
+            Console.WriteLine("Saldo atual: R$" + Saldo);
+            Console.WriteLine();
+        }
+        public void VerExtrato()
+        {
+            foreach (Movimentacao movimentacao in Movimentacoes)
+            {
+                Console.WriteLine("Valor: " + movimentacao.Valor);
+                Console.WriteLine("Tipo: " + movimentacao.Tipo);
+                Console.WriteLine();
+            }
+        }
+        public void Transferir(ContaCorrente contaDestino, double valor, string tipo)
+        {
+            Sacar(valor, tipo);
+            contaDestino.Depositar(valor, tipo);
+
+            Console.WriteLine($"Valor enviado: R${valor}");
+        }
+    }
+}
